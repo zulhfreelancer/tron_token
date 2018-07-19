4d5fe97250bb0d6b40109cdf80f2551d1e480a9b
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-07-19 12:38:16 +0000

    Using go-config package

--- go/main.go
+++ go/main.go
@@ -6,20 +6,32 @@ import (
 
   "github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient" // help us connect to node
+	"github.com/ethereum/go-ethereum/ethclient"
+  "github.com/micro/go-config"
+	"github.com/micro/go-config/source/file"
 )
 
+type Config struct {
+  RpcServer string `json:"rpc_server"`
+  TokenAddress string `json:"token_address"`
+  TestAccount string `json:"test_account"`
+}
+
 func main() {
-  conn, err := ethclient.Dial("http://127.0.0.1:7545")
+  config.Load(file.NewSource(file.WithPath("config.json")))
+  var conf Config
+  config.Scan(&conf)
+
+  conn, err := ethclient.Dial(conf.RpcServer)
   if err != nil {
     log.Fatalf("Failed to connect to Ethereum node: %v", err)
   }
 
-  contract, err := NewTronToken(common.HexToAddress("0x2c2b9c9a4a25e24b174f26114e8926a9f2128fe4"), conn)
+  contract, err := NewTronToken(common.HexToAddress(conf.TokenAddress), conn)
   if err != nil {
     log.Fatalf("Failed to create contract instance: %v", err)
   }
 
-  bal, _ := contract.BalanceOf(&bind.CallOpts{}, common.HexToAddress("0x627306090abaB3A6e1400e9345bC60c78a8BEf57"))
+  bal, _ := contract.BalanceOf(&bind.CallOpts{}, common.HexToAddress(conf.TestAccount))
   fmt.Println(bal)
 }
